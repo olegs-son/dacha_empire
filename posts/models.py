468b8f7f486177d29199612969994d1711863d79
Petrov.N.O <petrov.n.o.sp@gmail.com>
2023-01-15 19:50:48 +0300

    Рабочий вариант

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    fname = models.CharField(max_length=20)
+    lname = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=20)
+    email = models.TextField()
+    password = models.TextField()
+
+    def __str__(self) -> str:
+        return f"{self.fname} {self.lname}({self.id})"
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    published_by = models.ForeignKey("User", on_delete=models.CASCADE)
+    date_published = models.DateField()
+    date_start = models.DateField()
+    date_end = models.DateField()
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Post
 
-# Create your views here.
+def index(request):
+    template = loader.get_template('posts/index.html')
+    context = {
+        'posts_list': Post.objects.all()[::-1]
+    }
+    return HttpResponse(template.render(context, request))
+
+def registration(request):
+    pass
+
+def detail_post(request, post_id):
+    post = Post.objects.filter(id=1)
+    result = "ok"
+    return HttpResponse(result)
